05386d56c67d68cd3a36086b0438f0e3d578a7e4
JessCai06 <115125748+JessCai06@users.noreply.github.com>
2025-03-19 12:29:59 -0400

    more button in fun

--- static/script.js
+++ static/script.js
@@ -28,6 +28,12 @@ function greetingFunc() {
 
     E.innerHTML = `${greeting}! I am Jessica`;
 }
+function validate() {
+                const E = document.getElementById("message");
+                    if (E.innerHTML == show_more) {
+                        E.innerHTML = "<p style=\"red\">validated!<\p>";
+                    }
+                }
 
 function reveal(){
     const show_more = "I like coffee and cornbread.";
@@ -41,8 +47,21 @@ function reveal(){
         E.innerHTML = show_more;
         button.textContent = "show less";
     }
-
 }
-// Ensure the function runs when the page loads
+function show_more_fun(){
+    alert("hi");
+    const show_more = "<li>anything outdoors really - hiking camping biking</li><li>binging movies i've already watched with my friends who haven't seen it</li><li>cooking for other people</li><li>going to antique shops and flea markets</li><li>snorkeling hehe</li>";
+    const button = document.getElementById("showmore_fun");
+    const E = document.getElementById("fun_list_id");
+    if (E.innerHTML == show_more){
+        E.innerHTML = "";
+        button.textContent = "show more";
+    }
+    else{
+        E.innerHTML = show_more;
+        button.textContent = "show less";
+    }
+}
+
 window.onload = greetingFunc;
 window.onload = addYear;
