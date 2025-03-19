69bba59f9d901398fb847774120861e8c2281299
JessCai06 <115125748+JessCai06@users.noreply.github.com>
2025-03-19 13:30:25 -0400

    fixed small bugs

--- static/script.js
+++ static/script.js
@@ -14,24 +14,25 @@ function greetingFunc() {
     var h = d.getHours();
     const E = document.getElementById("greetings");
     let greeting = "";
+    if (E!= null){
+        if (h < 5) {
+            greeting = "Good night";
+        } else if (h < 12) {
+            greeting = "Good morning";
+        } else if (h < 18) {
+            greeting = "Good afternoon";
+        } else if (h < 20) {
+            greeting = "Good evening";
+        } else {
+            greeting = "Good night";
+        }
 
-    if (h < 5) {
-        greeting = "Good night";
-    } else if (h < 12) {
-        greeting = "Good morning";
-    } else if (h < 18) {
-        greeting = "Good afternoon";
-    } else if (h < 20) {
-        greeting = "Good evening";
-    } else {
-        greeting = "Good night";
-    }
-
-    E.innerHTML = `${greeting}! I am Jessica`;
+        E.innerHTML = `${greeting}! I am Jessica`;
+}
 }
 function validate() {
                 const E = document.getElementById("message");
-                    if (E.innerHTML == show_more) {
+                    if (E!= null && E.innerHTML == show_more) {
                         E.innerHTML = "<p style=\"red\">validated!<\p>";
                     }
                 }
@@ -40,27 +41,31 @@ function reveal(){
     const show_more = "I like coffee and cornbread.";
     const button = document.getElementById("showmore");
     const E = document.getElementById("fun show more text");
-    if (E.innerHTML == show_more){
-        E.innerHTML = "";
-        button.textContent = "show more";
-    }
-    else{
-        E.innerHTML = show_more;
-        button.textContent = "show less";
-    }
+    if (E!= null && button!= null){
+        if (E.innerHTML == show_more){
+            E.innerHTML = "";
+            button.textContent = "show more";
+        }
+        else{
+            E.innerHTML = show_more;
+            button.textContent = "show less";
+        }
+}
 }
 function validate(){
     const E = document.getElementById("name_input_contact");
     const B = document.getElementById("email_input_contact");
-    if (E.value != "" && B.value!="")
-    {
-        const M = document.getElementById("validate_message");
-        M.innerHTML= "Your form was sucessfully submitted ";
-    }
-    else{
-        const M = document.getElementById("validate_message");
-        M.innerHTML= "Please fix your form";
-    }
+    if (E!= null && B!= null){
+        if (E.value != "" && B.value!="")
+        {
+            const M = document.getElementById("validate_message");
+            M.innerHTML= "Your form was sucessfully submitted ";
+        }
+        else{
+            const M = document.getElementById("validate_message");
+            M.innerHTML= "Please fix your form";
+        }
+}
     console.log("this is E"+E.innerHTML.value);
 }
 
